2b1621701720b0994c90ad96c8a791ed0921a68e
Santiago <santiago_figueroa_@hotmail.com>
2025-05-01 11:44:27 +0100

    Add logging enhancements and project structure
    Updated `ASF.Logger.csproj` to include a "Services" folder.
    Refactored `ASFLogger` to a static class and added an
    `ExternalLogger` property for flexible logging. Introduced
    `Log` and `LogException` methods for improved logging
    functionality.

--- ASF.Logger/ASFLogger.cs
+++ ASF.Logger/ASFLogger.cs
@@ -1,7 +1,33 @@
-﻿namespace ASF.Logger
+﻿using ASF.Logger.Models;
+
+namespace ASF.Logger
 {
-    public class ASFLogger
+    public static class ASFLogger
     {
+        public static Action<Entry>? ExternalLogger { get; set; }
+
+        public static void Log(string message, Severity severity = Severity.Info, string? user = null, string? context = null, Exception? ex = null)
+        {
+            var logEntry = new Entry
+            {
+                Message = message,
+                Severity = severity,
+                User = user,
+                Context = context,
+                Exception =  ex?.Message + ex?.InnerException + ex?.StackTrace // Only included information essential for debugging
+            };
+
+            // Console output
+            Console.WriteLine($"[{logEntry.Timestamp:u}] [{logEntry.Severity}] {logEntry.Message}");
+
+            // Forward to custom external logger (e.g., PostgreSQL)
+            ExternalLogger?.Invoke(logEntry);
+        }
+
+        public static void LogException(Exception ex, string context = "", string? user = null)
+        {
+            Log(ex.Message, Severity.Error, user, context, ex);
+        }
 
     }
 }
